b476585e9c3a9cc041e237b66854c87eca952aa1
aaronr24468 <aaronr24468@gmail.com>
2026-08-11 10:25:55 -0600

    change origin cors

--- src/index.mjs
+++ src/index.mjs
@@ -19,7 +19,7 @@ const server = createServer(app);
 
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ferrepalweb.onrender.com',
     credentials: true
 }));
 app.use(cookieParser());
